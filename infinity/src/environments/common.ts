19c192ca099376733b150b41a399fa7e7df86709
El hocine Takouert <etakouer@c2r2p1.42nice.fr>
2022-03-15 15:47:29 +0100

    first

---
+++ infinity/src/environments/common.ts
@@ -0,0 +1,16 @@
+const HOST = 'localhost';
+// test
+//const HOST = '10.12.2.1';
+
+export const commonEnv = {
+  API_INFINITY: 'http://' + HOST + ':3000',
+  API_GATAWAY: 'ws://' + HOST + ':3002',
+  GAME_GATEWAY: 'ws://' + HOST + ':3000',
+  FORTY_TWO_ID:
+    '5c500a747105868ffa2bb6e7fe113ebc7339c75c2bbba37c88ccef7519d77f04',
+  //test
+  //FORTY_TWO_ID:
+  //  '6a90e4f3da9a7b5f6459fa0c21cd0c1da6572db90bf798cbecaf3f26291dbdb7',
+  INF_HOST: HOST,
+  INF_PORT: '4200',
+};
